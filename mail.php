d082436c70d083af55f626560d301bc590e414cc
DmitriyPoga <35847269+DmitriyPoga@users.noreply.github.com>
2018-08-31 13:04:54 +0300

    ВАП

--- mail.php
+++ mail.php
@@ -1,12 +1,23 @@
+<<<<<<< HEAD
+ <?php
+$to = "dmitri.dwp@mail.ru"; // емайл получателя данных из формы 
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла 
+$message = "Ваше имя: ".$_GET['name']."<br>";//присвоить переменной значение, полученное из формы name=name
+  $message .= "E-mail: ".$_GET['email']."<br>"; //полученное из формы name=email
+$message .= "Номер телефона: ".$_GET['phone']."<br>"; //полученное из формы name=phone
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+=======
 <?php 
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
+$name = $_GET['user_name'];
+$phone = $_GET['user_phone'];
+$email = $_GET['user_email'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
@@ -37,4 +48,5 @@ if(!$mail->send()) {
 } else {
     header('location: thank-you.html');
 }
+>>>>>>> 11487fe2c7c4b0a3597c485bcb9d9bd0759ff5b5
 ?>
