11487fe2c7c4b0a3597c485bcb9d9bd0759ff5b5
DmitriyPoga <35847269+DmitriyPoga@users.noreply.github.com>
2018-08-31 11:31:41 +0300

    ttttttttt

--- mail.php
+++ mail.php
@@ -13,13 +13,13 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'applicationkm@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Poimenidj2018'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('applicationkm@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('dmitri.dwp@mail.ru');     // Кому будет уходить письмо 
+$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -28,7 +28,7 @@ $mail->addAddress('dmitri.dwp@mail.ru');     // Кому будет уходит
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с koxivmarketing.com';
+$mail->Subject = 'Заявка с тестового сайта';
 $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
 $mail->AltBody = '';
 
